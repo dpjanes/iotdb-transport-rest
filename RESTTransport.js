c0c6eaea6f83ed7ae2e1432d9922de50801f339a
David Janes <davidjanes@davidjanes.com>
2015-06-02 07:26:15 -0400

    dist

--- RESTTransport.js
+++ RESTTransport.js
@@ -168,10 +168,13 @@ RESTTransport.prototype._setup_app_thing_band = function() {
     });
 
     self.native.put(channel, function(request, response) {
+        var d = request.body;
+        _.timestamp.update(d);
+
         self._emitter.emit("updated", {
             id: request.params.id, 
             band: request.params.band, 
-            value: request.body,
+            value: d,
         });
 
         var rd = {
